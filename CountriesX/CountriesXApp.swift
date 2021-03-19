2a2270d05a0c009509c5ebc0a9b9590fe7e963fc
Ces <cesmejia@gruposeara.com>
2021-03-18 21:34:56 -0600

    Initial Commit

---
+++ CountriesX/CountriesXApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesXApp.swift
+//  CountriesX
+//
+//  Created by Ces Mejia on 18/03/21.
+//
+
+import SwiftUI
+
+@main
+struct CountriesXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

c6f24011e48ac205d5e9e19d8b55584d1d090ff5
Ces <cesmejia@gruposeara.com>
2021-03-21 10:34:44 -0600

    add CountriesWebRepository

---
+++ CountriesX/Repositories/CountriesWebRepository.swift
@@ -0,0 +1,73 @@
+//
+//  CountriesWebRepository.swift
+//  CountriesX
+//
+//  Created by Ces Mejia on 21/03/21.
+//
+
+import Foundation
+import Combine
+
+protocol CountriesWebRepository: WebRepository {
+    func loadCountries() -> AnyPublisher<[Country], Error>
+    func loadCountryDetails(country: Country) -> AnyPublisher<Country.Details.Intermediate, Error>
+}
+
+struct RealCountriesWebRepository: CountriesWebRepository {
+    var session: URLSession
+    var baseURL: String
+    var bgQueue = DispatchQueue(label: "bg_parse_queue")
+    
+    func loadCountries() -> AnyPublisher<[Country], Error> {
+        call(endpoint: API.allCountries)
+    }
+    
+    func loadCountryDetails(country: Country) -> AnyPublisher<Country.Details.Intermediate, Error> {
+        let request: AnyPublisher<[Country.Details.Intermediate], Error> = call(endpoint: API.countryDetails(country))
+        return request
+            .tryMap { array -> Country.Details.Intermediate in
+                guard let details = array.first
+                    else { throw APIError.unexpectedResponse }
+                return details
+            }
+            .eraseToAnyPublisher()
+    }
+}
+
+// MARK: - Endpoints
+
+extension RealCountriesWebRepository {
+    enum API {
+        case allCountries
+        case countryDetails(Country)
+    }
+}
+
+extension RealCountriesWebRepository.API: APICall {
+    var path: String {
+        switch self {
+        case .allCountries:
+            return "/all"
+        case let .countryDetails(country):
+            let encodedName = country.name.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+            return "/name/\(encodedName ?? country.name)"
+        }
+    }
+    
+    var method: String {
+        switch self {
+        case .allCountries, .countryDetails:
+            return "GET"
+        }
+    }
+    
+    var headers: [String : String]? {
+        return ["Accept": "application/json"]
+    }
+    
+    func body() throws -> Data? {
+        return nil
+    }
+    
+    
+}
---
+++ CountriesXTests/Repositories/CountriesWebRepositoryTests.swift
@@ -0,0 +1,24 @@
+//
+//  CountriesWebRepositoryTests.swift
+//  CountriesXTests
+//
+//  Created by Ces Mejia on 21/03/21.
+//
+
+import XCTest
+import Combine
+@testable import CountriesX
+
+class CountriesWebRepositoryTests: XCTestCase {
+    
+//    private var sut = 
+
+    override func setUp() {
+        
+    }
+
+    override func tearDown() {
+        
+    }
+    
+}

ca4706ba98f64b622f4cfbc56dbc3093e88c30c4
Ces <cesmejia@gruposeara.com>
2021-03-21 17:38:19 -0600

    add all CoreDataStack tests

--- CountriesX/Models/Models+CoreData.swift
+++ CountriesX/Models/Models+CoreData.swift
@@ -22,3 +22,50 @@ extension Locale {
         return String(identifier.prefix(2))
     }
 }
+
+
+extension Country {
+    
+    @discardableResult
+    func store(in context: NSManagedObjectContext) -> CountryMO? {
+        guard let country = CountryMO.insertNew(in: context)
+            else { return nil }
+        country.name = name
+        country.alpha3code = alpha3Code
+        country.population = Int32(population)
+        country.flagURL = flag?.absoluteString
+        let translations = self.translations
+            .compactMap { (locale, name) -> NameTranslationMO? in
+                guard let name = name,
+                    let translation = NameTranslationMO.insertNew(in: context)
+                else { return nil }
+                translation.name = name
+                translation.locale = locale
+                return translation
+            }
+        country.nameTranslations = NSSet(array: translations)
+        return country
+    }
+    
+    init?(managedObject: CountryMO) {
+        guard let nameTranslations = managedObject.nameTranslations
+            else { return nil }
+        let translations: [String: String?] = nameTranslations
+            .toArray(of: NameTranslationMO.self)
+            .reduce([:], { (dict, record) -> [String: String?] in
+                guard let locale = record.locale, let name = record.name
+                    else { return dict }
+                var dict = dict
+                dict[locale] = name
+                return dict
+            })
+        guard let name = managedObject.name,
+            let alpha3code = managedObject.alpha3code
+            else { return nil }
+        
+        self.init(name: name, translations: translations,
+                  population: Int(managedObject.population),
+                  flag: managedObject.flagURL.flatMap { URL(string: $0) },
+                  alpha3Code: alpha3code)
+    }
+}

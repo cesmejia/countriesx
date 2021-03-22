de1095a32cf2813a80b4cb5ed35f347de4bd2781
Ces <cesmejia@gruposeara.com>
2021-03-22 08:02:47 -0600

    add RealCountriesDBRepository

--- CountriesX/Models/Models+CoreData.swift
+++ CountriesX/Models/Models+CoreData.swift
@@ -23,6 +23,60 @@ extension Locale {
     }
 }
 
+extension Country.Details {
+    
+    init?(managedObject: CountryDetailsMO) {
+        guard let capital = managedObject.capital
+            else { return nil }
+        
+        let currencies = (managedObject.currencies ?? NSSet())
+            .toArray(of: CurrencyMO.self)
+            .compactMap { Country.Currency(managedObject: $0) }
+        
+        let borders = (managedObject.borders ?? NSSet())
+            .toArray(of: CountryMO.self)
+            .compactMap { Country(managedObject: $0) }
+            .sorted(by: { $0.name < $1.name })
+        
+        self.init(capital: capital, currencies: currencies, neighbors: borders)
+    }
+}
+
+extension Country.Details.Intermediate {
+    
+    @discardableResult
+    func store(in context: NSManagedObjectContext,
+               country: CountryMO, borders: [CountryMO]) -> CountryDetailsMO? {
+        guard let details = CountryDetailsMO.insertNew(in: context)
+            else { return nil }
+        details.capital = capital
+        let storedCurrencies = currencies.compactMap { $0.store(in: context) }
+        details.currencies = NSSet(array: storedCurrencies)
+        details.borders = NSSet(array: borders)
+        country.countryDetails = details
+        return details
+    }
+}
+
+extension Country.Currency {
+    
+    init?(managedObject: CurrencyMO) {
+        guard let code = managedObject.code,
+            let name = managedObject.name
+            else { return nil }
+        self.init(code: code, symbol: managedObject.symbol, name: name)
+    }
+    
+    @discardableResult
+    func store(in context: NSManagedObjectContext) -> CurrencyMO? {
+        guard let currency = CurrencyMO.insertNew(in: context)
+            else { return nil }
+        currency.code = code
+        currency.name = name
+        currency.symbol = symbol
+        return currency
+    }
+}
 
 extension Country {
     
@@ -68,4 +122,6 @@ extension Country {
                   flag: managedObject.flagURL.flatMap { URL(string: $0) },
                   alpha3Code: alpha3code)
     }
+    
+    
 }

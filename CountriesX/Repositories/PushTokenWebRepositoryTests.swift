10470954dc1046cbf67e38355f9233c2004273df
Cesar  Mejia <rasec8me>
2021-03-27 18:54:41 -0600

    add PushTokenWebRepositoryTests

---
+++ CountriesX/Repositories/PushTokenWebRepositoryTests.swift
@@ -0,0 +1,36 @@
+//
+//  PushTokenWebRepositoryTests.swift
+//  CountriesXTests
+//
+//  Created by Cesar Mejia Valero on 27/03/21.
+//
+
+import XCTest
+import Combine
+@testable import CountriesX
+
+class PushTokenWebRepositoryTests: XCTestCase {
+
+    private var sut: RealPushTokenWebRepository!
+    private var cancelBag = CancelBag()
+    
+    override func setUp() {
+        sut = RealPushTokenWebRepository(session: .mockedResponsesOnly,
+                                         baseURL: "https://test.com")
+    }
+    
+    override func tearDown() {
+        cancelBag = CancelBag()
+    }
+    
+    func test_register() {
+        let exp = XCTestExpectation(description: #function)
+        sut.register(devicePushToken: Data())
+            .sinkToResult { result in
+                result.assertSuccess()
+                exp.fulfill()
+            }
+            .store(in: cancelBag)
+        wait(for: [exp], timeout: 0.1)
+    }
+}

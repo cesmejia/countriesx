a83414b55b9ab443b4679a10c92f2b69bc91681c
Cesar  Mejia <rasec8me>
2021-03-28 21:37:03 -0600

    add tests for all services

--- CountriesX/Services/UserPermissionsService.swift
+++ CountriesX/Services/UserPermissionsService.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UserNotifications
 
 enum Permission {
     case pushNotifications
@@ -17,5 +18,93 @@ extension Permission {
         case notRequested
         case granted
         case denied
+    }
+}
+
+protocol UserPermissionsService: class {
+    func resolveStatus(for permission: Permission)
+    func request(permission: Permission)
+}
+
+// MARK: - RealUserPermissionsService
+
+final class RealUserPermissionsService: UserPermissionsService {
+    
+    private let appState: Store<AppState>
+    private let openAppSettings: () -> Void
+    
+    init(appState: Store<AppState>, openAppSettings: @escaping () -> Void) {
+        self.appState = appState
+        self.openAppSettings = openAppSettings
+    }
+    
+    func resolveStatus(for permission: Permission) {
+        let keyPath = AppState.permissionKeyPath(for: permission)
+        let currentStatus = appState[keyPath]
+        guard currentStatus == .unknown else { return }
+        let onResolve: (Permission.Status) -> Void = { [weak appState] status in
+            appState?[keyPath] = status
+        }
+        switch permission {
+        case .pushNotifications:
+            pushNotificationsPermissionStatus(onResolve)
+        }
+    }
+    
+    func request(permission: Permission) {
+        let keyPath = AppState.permissionKeyPath(for: permission)
+        let currentStatus = appState[keyPath]
+        guard currentStatus != .denied else {
+            openAppSettings()
+            return
+        }
+        switch permission {
+        case .pushNotifications:
+            requestPushNotificationsPermission()
+        }
+    }
+}
+    
+// MARK: - Push Notifications
+
+extension UNAuthorizationStatus {
+    var map: Permission.Status {
+        switch self {
+        case .denied: return .denied
+        case .authorized: return .granted
+        case .notDetermined, .provisional, .ephemeral: return .notRequested
+        @unknown default: return .notRequested
+        }
+    }
+}
+
+private extension RealUserPermissionsService {
+    
+    func pushNotificationsPermissionStatus(_ resolve: @escaping (Permission.Status) -> Void) {
+        let center = UNUserNotificationCenter.current()
+        center.getNotificationSettings { settings in
+            DispatchQueue.main.async {
+                resolve(settings.authorizationStatus.map)
+            }
+        }
+    }
+    
+    func requestPushNotificationsPermission() {
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound]) { (isGranted, error) in
+            DispatchQueue.main.async {
+                self.appState[\.permissions.push] = isGranted ? .granted : .denied
+            }
+        }
+    }
+}
+
+// MARK: -
+
+final class StubUserPermissionsService: UserPermissionsService {
+    
+    func resolveStatus(for permission: Permission) {
+    }
+    func request(permission: Permission) {
     }
 }
